2356c02d498f486d5fb4d40fe6f6af9627198077
jorge Valencia <jvalenciam@val-corp.com>
2021-07-15 12:05:30 -0500

    Homework 03-JS-II

--- 03-JS-II/homework/topractice.js
+++ 03-JS-II/homework/topractice.js
@@ -9,7 +9,7 @@ function obtenerMayor(x, y) {
           else{return Math.round(Math.random()*2);
         }
   }    
-}console.log(obtenerMayor(2,2));
+};
 
 
 function mayoriaDeEdad(edad) {
@@ -46,12 +46,176 @@ function saludo(idioma) {
   if (idioma==="aleman"){return "Guten Tag!";}
   else{if(idioma==="mandarin"){return "Ni Hao!";}
        else{if(idioma==="ingles"){return "Hello!";}
-            else{return "undefined";}
-      
-      
+            else{return "undefined";}     
       }
-  
-  }
-  
-  
-  }console.log(saludo("español"));
+    }
+}console.log(saludo("español"));
+
+
+function verasidad(dato){
+  if (dato){
+    console.log("es tryly");
+  }else{console.log("es falsy");}
+}console.log (verasidad(0));
+
+
+console.log(1>2 || 0>2);
+
+
+
+function delunoal10for(){
+  for(var i=0; i<=10;i=i+2){
+  console.log(i);};
+};
+
+delunoal10for();
+
+function colors(color) {
+  //La función recibe un color. Devolver el string correspondiente:
+  //En caso que el color recibido sea "blue", devuleve --> "This is blue"
+  //En caso que el color recibido sea "red", devuleve --> "This is red"
+  //En caso que el color recibido sea "green", devuleve --> "This is green"
+  //En caso que el color recibido sea "orange", devuleve --> "This is orange"
+  //Caso default: devuelve --> "Color not found"
+  //Usar el statement Switch.
+  if(color==="blue"){return "This is blue";}
+  else if (color==="red"){return "This is red";}
+  else if (color==="green"){return "This is green";}
+  else if (color==="orange"){return "This is orange";}
+  else{return "Color not found";}
+
+  }console.log(colors("orange"));
+
+
+  function delunoal5while(){
+  var count=0;
+  var suma=0;
+  while(count < 6){
+    suma = suma+count;
+    count++}
+  return suma;}
+  console.log(delunoal5while());
+
+
+  function esDiezOCinco(numero) {
+    // Devuelve "true" si "numero" es 10 o 5
+    // De lo contrario, devuelve "false"
+    // Tu código:
+    if(numero==10||numero==5){return true;}
+    else {return false;}
+  }console.log(esDiezOCinco(5));
+
+
+
+  function colors(color) {
+    //La función recibe un color. Devolver el string correspondiente:
+    //En caso que el color recibido sea "blue", devuleve --> "This is blue"
+    //En caso que el color recibido sea "red", devuleve --> "This is red"
+    //En caso que el color recibido sea "green", devuleve --> "This is green"
+    //En caso que el color recibido sea "orange", devuleve --> "This is orange"
+    //Caso default: devuelve --> "Color not found"
+    //Usar el statement Switch.
+       switch(color) {
+      case "blue":
+        return "This is blue";
+        break;
+      case "red":
+        return "This is red";
+        break;
+      case "green":
+        return "This is green";
+          break;
+      case "orange":
+        return "This is orange";
+            break;
+      
+      default:
+        return "Color not found";
+    }}console.log(colors("red"));
+
+
+    function estaEnRango(numero) {
+      // Devuelve "true" si "numero" es menor que 50 y mayor que 20
+      // De lo contrario, devuelve "false"
+      // Tu código:
+    if(numero<50 && numero>20){
+      return true;}
+      else {return false;}
+    }console.log(estaEnRango(30));
+
+
+    function esEntero(numero) {
+      // Devuelve "true" si "numero" es un entero (int/integer)
+      // Ejemplo: 0.8 -> false
+      // Ejemplo: 1 -> true
+      // Ejemplo: -10 -> true
+      // De lo contrario, devuelve "false"
+      // Pista: Puedes resolver esto usando `Math.floor`
+      // Tu código:
+    if(numero===Math.floor(numero)){return true;}
+    else {return false;}
+    
+    }console.log(esEntero(1));
+
+    function fizzBuzz(numero) {
+      // Si "numero" es divisible entre 3, devuelve "fizz"
+      // Si "numero" es divisible entre 5, devuelve "buzz"
+      // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+      // De lo contrario, devuelve el numero
+      if(numero%3===0 && numero%5===0){return "fizzbuzz";}
+      else if(numero%5===0){return "buzz";}
+      else if(numero%3===0){return "fizz";}
+      else{return numero;}
+    
+    
+    }console.log(fizzBuzz(9));
+
+
+    function operadoresLogicos(num1, num2, num3) {
+      //La función recibe tres números distintos. 
+      //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+      //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+      //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+      //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+      //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+     if((num1===0||num2===0||num3)===0){return "Error";}
+     else if((num1<0||num2<0||num3)<0){return "Hay negativos";}
+     else if(num1>num2 && num1>num3 && num1>0){return "Número 1 es mayor y positivo";}
+     else if(num3>num1 && num3>num2){ return num3+1;}
+     else{return false;};
+    
+    }console.log(operadoresLogicos(10,2,11));
+
+
+    function esVerdadero(valor){
+      //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
+      //si su valor es true y “Soy falso” si su valor es false.
+      //Escribe tu código aquí
+      if (valor===true){return "Soy Verdadero";}
+      else{return "Soy falso";}
+    
+    };
+
+
+    function doWhile(numero) {
+      //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
+      //Retornar el valor final.
+      //Usar el bucle do ... while.
+    var iteration=0;
+    do{
+      numero=numero+5;
+      iteration++;
+    }while (iteration<8);
+    return numero;
+    
+    }console.log (doWhile(3));
+
+
+    function tieneTresDigitos(numero){
+      //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+      //Escribe tu código aquí
+      numero=numero.toString();
+      if(numero.length===3){return "true";}
+      else{return "false";}
+      
+    }console.log(tieneTresDigitos(10));
